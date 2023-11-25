23386ae998f6e50a793eb0773daa5be2fe866caa
KaungKhantSiThu <64033794+KaungKhantSiThu@users.noreply.github.com>
2023-11-25 16:51:26 +0700

    Replace preview data in DiscoverMovieView

---
+++ Cinephile/Media Tracker/Detail/View/CastView.swift
@@ -0,0 +1,18 @@
+//
+//  CastView.swift
+//  Cinephile
+//
+//  Created by Kaung Khant Si Thu on 25/11/2023.
+//
+
+import SwiftUI
+
+struct CastView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CastView()
+}
---
+++ Cinephile/Preview Content/PreviewLoader.swift
@@ -0,0 +1,8 @@
+//
+//  PreviewLoader.swift
+//  Cinephile
+//
+//  Created by Kaung Khant Si Thu on 25/11/2023.
+//
+
+import Foundation
