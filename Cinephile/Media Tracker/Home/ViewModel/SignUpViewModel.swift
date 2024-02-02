ef002422367adf23090dacdd181a1b5cef445163
Kelvin Kaung <u6511753@au.edu>
2024-02-02 22:39:03 +0700

    Signup view UI changed

--- Cinephile/Media Tracker/Home/ViewModel/SignUpViewModel.swift
+++ Cinephile/Media Tracker/Home/ViewModel/SignUpViewModel.swift
@@ -1,38 +1,47 @@
 import Foundation
+import Alamofire
+
+public struct RegisterModel : Codable, Sendable {
+    var username : String
+    var email : String
+    var password : String
+    var agreement : Bool
+    var reason : String
+    var locale : String
+    
+    init(username: String, email: String, password: String, agreement: Bool = true, reason: String = "test", locale: String = "en") {
+        self.username = username
+        self.email = email
+        self.password = password
+        self.agreement = agreement
+        self.reason = reason
+        self.locale = locale
+    }
+}
 
 class SignUpViewModel {
-    static let shared = SignUpViewModel()
-
-    func signUp(user: SignUpModel, completion: @escaping (Result<Data, Error>) -> Void) {
-        guard let url = URL(string: "https://polar-brushlands-19893-4c4dfbb9419d.herokuapp.com/api/v1/accounts") else {
-            return
-        }
+    static var shared = SignUpViewModel()
+    
+    func createData(reigsterModel : RegisterModel) {
+        let header : HTTPHeaders = [
+            .authorization(bearerToken: "x-t8FUzquP4JsihXzWhzsbAFN1k7zoysO-DdImGMcwM"),
+            .contentType("application/json")
+        ]
+        guard let url = URL(string: "https://polar-brushlands-19893-4c4dfbb9419d.herokuapp.com/api/v1/accounts") else { return }
         
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-
-        do {
-            let encoder = JSONEncoder()
-            let userData = try encoder.encode(user)
-            request.httpBody = userData
-
-            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-                if let error = error {
-                    completion(.failure(error))
-                    return
+        AF.request(url, method: .post, parameters: reigsterModel, encoder: JSONParameterEncoder.default, headers: header).response { response in
+            debugPrint(response)
+            switch response.result {
+            case .success(let data):
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data!, options: [])
+                    print(json)
+                } catch {
+                    print(error.localizedDescription)
                 }
-
-                guard let data = data else {
-                    return
-                }
-
-                completion(.success(data))
+            case .failure(let err):
+                print(err.localizedDescription)
             }
-
-            task.resume()
-        } catch {
-            completion(.failure(error))
         }
     }
 }
